dd208a9b3d9349eae4f7113d514c741bc71fa49e
Alisa Borkowska <alisa.borkowska@gmail.com>
2024-06-16 18:56:17 +0200

    added progress steps JS code

--- 2 progress steps/script.js
+++ 2 progress steps/script.js
@@ -9,8 +9,7 @@ next.addEventListener('click', () => {
   if (currentActive > circles.length) {
     currentActive = circles.length
   }
-
-  console.log(currentActive)
+  update()
 })
 
 prev.addEventListener('click', () => {
@@ -18,6 +17,27 @@ prev.addEventListener('click', () => {
   if (currentActive < 1) {
     currentActive = 1
   }
-
-  console.log(currentActive)
+  update()
 })
+
+const update = () => {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active')
+    } else {
+      circle.classList.remove('active')
+    }
+  })
+  const actives = document.querySelectorAll('.active')
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%'
+
+  if (currentActive === 1) {
+    prev.disabled = true
+  } else if (currentActive === circles.length) {
+    next.disabled = true
+  } else {
+    prev.disabled = false
+    next.disabled = false
+  }
+}
