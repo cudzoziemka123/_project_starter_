428a1a8a6a66730297f0ae4d56fbc00662e2e7e6
Alisa <alisa.borkowska@gmail.com>
2024-10-04 15:49:47 +0200

    drink water project

---
+++ 16 drink water/script.js
@@ -0,0 +1,25 @@
+const smallcups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+
+smallcups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlightCups(idx));
+});
+
+function highlightCups(idx) {
+  if (
+    smallcups[idx].classList.contains("full") &&
+    !smallcups[idx].nextElementSibling.classList.contains("full")
+  ) {
+    idx--;
+  }
+
+  smallcups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+}
