cef3f19be8b134e668753bde920a07510cf95673
Alisa <alisa.borkowska@gmail.com>
2024-10-08 12:25:46 +0200

    added big Cup functionality

--- 16 drink water/script.js
+++ 16 drink water/script.js
@@ -2,7 +2,7 @@ const smallcups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
-
+updateBigCup();
 smallcups.forEach((cup, idx) => {
   cup.addEventListener("click", () => highlightCups(idx));
 });
@@ -22,4 +22,26 @@ function highlightCups(idx) {
       cup.classList.remove("full");
     }
   });
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallcups.length;
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
