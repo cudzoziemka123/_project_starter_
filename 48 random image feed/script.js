21f9aec29589bdfbe527d341bc3dd7c824b8aca7
Alisa <alisa.borkowska@gmail.com>
2024-11-18 14:02:39 +0100

    random image feed

---
+++ 48 random image feed/script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+const picsumURL = "https://picsum.photos/";
+const rows = 10;
+
+for (let i = 0; i < rows * 3; i++) {
+  const img = document.createElement("img");
+  img.src = `${picsumURL}${getRandomSize()}`;
+  container.appendChild(img);
+}
+
+function getRandomSize() {
+  return `${getRandomNr()}/${getRandomNr()}`;
+}
+
+function getRandomNr() {
+  return Math.floor(Math.random() * 20) + 300;
+}
